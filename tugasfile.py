3156e72eaa9c961271ad72f8edc1b3c626399947
ALRISQY <98927707+ALRISQY@users.noreply.github.com>
2022-05-21 11:04:56 +0800

    Add files via upload

---
+++ tugasfile.py
@@ -0,0 +1,18 @@
+hasil=[]
+hasil_2=0
+while True:
+	nilai=int(input("masukan nilai="))
+	hasil_2 += nilai
+	hasil.append(nilai)
+	if nilai==0:
+		break
+	
+print(f"listnya nya={hasil}")
+print(f"nilai rata-ratanya adalah={hasil_2}")
+
+		
+
+
+	
+	
+	
